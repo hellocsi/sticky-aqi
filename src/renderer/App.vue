84b62d702b5750c436fba84788eea8289ebb9dad
Chang Si <csi@glitech.com>
2020-01-21 15:02:12 +0800

    创建初始版本

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'weather-widget'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
